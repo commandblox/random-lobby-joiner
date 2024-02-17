77db4ec2a39bee1931134f3b53dc7c1c4e67f6f6
BuildTools <unconfigured@null.spigotmc.org>
2024-02-17 17:49:29 -0500

    Add project files.

---
+++ randomLobbyJoiner.cs
@@ -0,0 +1,23 @@
+﻿using Splotch;
+using Splotch.Event;
+using HarmonyLib;
+
+namespace RandomLobbyJoiner
+{
+    public class RandomLobbyJoiner : SplotchMod
+    {
+        public override void OnLoad()
+        {
+            Logger.Log("hefhakwdn iehnfisoie");
+            EventManager.RegisterEventListener(typeof(Events));
+            Harmony.PatchAll(typeof(HarmonyPatches));
+        }
+    }
+
+    public static class Events
+    {
+    }
+
+    public static class HarmonyPatches
+    {
+}
